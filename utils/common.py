23a404bde203543628401b963b432f93374a748a
NFedunov <nickolayfedunov@gmail.com>
2018-12-05 08:10:28 +0300

    Revert "Предположительно пофиксено последовательное декодирование"
    This reverts commit b7232dc85b529f20809fac37cfa6edf937a8fe3a.

--- utils/common.py
+++ utils/common.py
@@ -1,11 +1,7 @@
 import math
 import numpy as np
-import timeit
-
 
 class UtilsCommon:
-
-    DEBUG = False
 
     @staticmethod
     def check_string_binary(str):
@@ -52,10 +48,7 @@ class UtilsCommon:
     @staticmethod
     def get_i_th_element_of_subfunc(i, flat, n):
         offset = 0
-
-    @staticmethod
-    def measure_perf(callback, times=1):
-        return timeit.timeit(callback, number=times)
+        
 
     @staticmethod
     def get_weight(var_int, lim=32):
@@ -108,8 +101,3 @@ class UtilsCommon:
     def np_array_from_ones(array, copy=True):
         vec_func = np.vectorize(UtilsCommon.from_one)
         return UtilsCommon.np_array_apply_to_each(array, vec_func, copy)
-
-    @staticmethod
-    def log(message):
-        if UtilsCommon.DEBUG:
-            print(message)
